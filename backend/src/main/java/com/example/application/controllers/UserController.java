e7ecb9b20a0683ce52397d7efe503be21a1d8028
Ravi Mourya <ravi.mourya@cimpress.com>
2025-12-15 23:14:04 +0530

    first commit.

---
+++ backend/src/main/java/com/example/application/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.example.application.controllers;
+
+import com.example.application.entities.User;
+import com.example.application.repositories.UserRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
